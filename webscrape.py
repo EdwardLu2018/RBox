d341c1af824ccf6a5233513384920caa716ec1b1
Edward <lued2018@gmail.com>
2018-11-10 15:11:00 -0500

    commit

--- webscrape.py
+++ webscrape.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 import time
+import json
 
 driver = webdriver.Chrome("/Users/Edward/Documents/chromedriver")
  
 url='https://edwardlu2018.github.io/RBox/'
 driver.get(url)
-scriptArray="""return Array.apply(0, new Array(localStorage.length)).map(function (o, i) { return localStorage.getItem(localStorage.key(i)); }
-				)""" 
+scriptArray="""return Array.apply(0, new Array(localStorage.length)).map(function (o, i) { return localStorage.getItem(localStorage.key(i)); })""" 
 
 while 1:
     result = driver.execute_script(scriptArray)
     print(result)
-    time.sleep(2)
+    time.sleep(1)
